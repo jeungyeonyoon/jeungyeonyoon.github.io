9c4f61bfeb6bf036148730f08f4e45c815819c53
jeungyeonyoon <38730476+jeungyeonyoon@users.noreply.github.com>
2018-07-08 00:23:50 -0600

    jQuery3

--- cit230/assignment/lesson-11/Lesson4/scripts/jqueryinsert.js
+++ cit230/assignment/lesson-11/Lesson4/scripts/jqueryinsert.js
@@ -2,21 +2,21 @@ $(function () {
     var box1 = $('.box1');
     var box2 = $('.box2');
 
-    function leftToRight() {
+    function LeftToRight() {
         if (box2.html() == '') {
             box2.html(box1.html());
             box1.html('');
         }
     }
 
-    function rightToLeft() {
-        if (box1.html() == "") {
+    $('#LeftToRight').click(LeftToRight)
+
+    function RightToLeft() {
+        if (box1.html() == '') {
             box1.html(box2.html());
-            box2.html("");
+            box2.html('');
         }
     }
 
-    $('#leftToRight').click(leftToRight);
-    $('#RightToLeft').click(rightToLeft);
-
+    $('#RightToLeft').click(RightToLeft)
 })
